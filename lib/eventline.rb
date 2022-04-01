663a60ae965d839aea13942b0074eade1e90bc55
gearnode <bryan.frimin@exograd.com>
2022-04-01 09:35:30 +0200

    Add missing require

--- lib/eventline.rb
+++ lib/eventline.rb
@@ -5,6 +5,7 @@ require "eventline/client"
 require "eventline/context"
 require "eventline/event"
 require "eventline/organization"
+require "eventline/account"
 
 module Eventline
   class Error < StandardError; end
